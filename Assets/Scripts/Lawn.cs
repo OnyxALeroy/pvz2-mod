41e54f68445c08f0a029684f051467514d56491e
Antoine Dupret (Onyx) <antoine.dupret@telecomnancy.net>
2025-03-29 15:54:29 +0100

    [Fix] Few adjustments, regarding tile sizes

--- Assets/Scripts/Lawn.cs
+++ Assets/Scripts/Lawn.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 
 public class Lawn : MonoBehaviour
 {
+    [SerializeField] Sprite lawnImage;
     [SerializeField] Image lawnBackground;
     [SerializeField] GameObject lawnGrid;
     [SerializeField] GameObject tilePrefab;
@@ -11,6 +12,7 @@ public class Lawn : MonoBehaviour
 
     void Start()
     {
+        lawnBackground.sprite = lawnImage;
         for (int i = 0; i < tileAmount; i++){
             GameObject tile = Instantiate(tilePrefab, lawnGrid.transform);
         }
