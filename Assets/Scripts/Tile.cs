57ee5c8acc0591bf793f0cef97d7bb043d512d2c
Antoine Dupret (Onyx) <antoine.dupret@telecomnancy.net>
2025-04-08 13:31:49 +0200

    [FEAT] Peashooter and Sunflower idle animations

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
+using System.Collections;
 
 public class Tile : MonoBehaviour
 {
     private bool isFree = true;
     public bool IsFree => isFree;
+    
+    private GameObject content;
 
     public void Plant(GameObject plant){
         isFree = false;
-        Instantiate(plant, transform.position, transform.rotation);
+        content = Instantiate(plant, transform.position, transform.rotation);
+        StartCoroutine(AdjustAfterLayout());
+    }
+
+    private IEnumerator AdjustAfterLayout()
+    {
+        yield return new WaitForEndOfFrame();
+
+        content.GetComponent<RectTransform>().anchoredPosition += new Vector2(0, -0.1f);
     }
 }
